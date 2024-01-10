adec02ce1bd5cb7057d008b20522a37e2a47e3d2
assetrakhmet <rakhmetovasset@gmail.com>
2024-01-10 02:39:13 +0000

    rds

---
+++ rds.tf
@@ -0,0 +1,27 @@
+# NOTE! There are some parts that you need to change and uncomment. Pay attention to comments.
+
+resource "aws_rds_cluster" "project" {
+  cluster_identifier        = "project"
+  availability_zones        = var.availability_zones
+  engine                    = "mysql"
+  db_cluster_instance_class = "db.m5d.large"
+  storage_type              = "io1"
+  allocated_storage         = 100
+  iops                      = 1000
+  master_username           = "test"
+  master_password           = "Techtorial123"
+  database_name             = "project"
+  db_subnet_group_name      = aws_db_subnet_group.private_subnet.name
+  skip_final_snapshot       = true
+  enable_http_endpoint      = true
+  # vpc_security_group_ids = aws_security_group.projectSG_RDS.id  
+  # Uncomment it when deploying on the final stage. It will automatically assign security group to this cluster.
+}
+
+resource "aws_db_subnet_group" "private_subnet" {
+  name        = "private_subnet_group"
+  subnet_ids  = aws_subnet.private_subnet[*].id
+  tags = {
+    name = "private subnet group"
+  }
+}
