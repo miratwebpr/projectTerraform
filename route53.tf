662376ea12f4aa94b879dd90b6b33902415c09bb
iamsalohiddin <samariddinov9313@gmail.com>
2024-01-12 03:18:36 +0000

    Updated hosted zones

--- route53.tf
+++ route53.tf
@@ -1,32 +1,48 @@
 resource "aws_route53_zone" "main" {
-  name = "salokhiddin.link"   # Replace it with your domain name
+  name = "salokhiddin.link" # Replace it with your dns name
 }
 
 resource "aws_route53_record" "writer" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "writer"
-  type    = "CNAME"
-  ttl     = 90
-  records = [aws_lb.projectLB.dns_name]  
+  name    = "writer" 
+  type    = "A" 
+
+  alias {
+      name                   = aws_lb.projectLB.dns_name
+      zone_id                = aws_lb.projectLB.zone_id
+      evaluate_target_health = true
+  }
 }
 resource "aws_route53_record" "reader1" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "reader1"
-  type    = "CNAME"
-  ttl     = 90
-  records = [aws_lb.projectLB.dns_name]  
+  name    = "reader1" 
+  type    = "A" 
+
+  alias {
+      name                   = aws_lb.projectLB.dns_name
+      zone_id                = aws_lb.projectLB.zone_id
+      evaluate_target_health = true
+  }
 }
 resource "aws_route53_record" "reader2" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "reader2"
-  type    = "CNAME"
-  ttl     = 90
-  records = [aws_lb.projectLB.dns_name]  
+  name    = "reader2" 
+  type    = "A" 
+
+  alias {
+      name                   = aws_lb.projectLB.dns_name
+      zone_id                = aws_lb.projectLB.zone_id
+      evaluate_target_health = true
+  }
 }
 resource "aws_route53_record" "wordpress" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "wordpress"
-  type    = "CNAME"
-  ttl     = 90
-  records = [aws_lb.projectLB.dns_name]
+  name    = "wordpress" 
+  type    = "A" 
+
+  alias {
+      name                   = aws_lb.projectLB.dns_name
+      zone_id                = aws_lb.projectLB.zone_id
+      evaluate_target_health = true
+  }
 }
